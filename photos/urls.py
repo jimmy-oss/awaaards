7282f90b99c8cc4ee4b3f66ddce68d8271a3a65f
jimmy-oss <jimmynjonge51@gmail.com>
2022-06-11 23:08:28 +0300

    configuring signin page

--- photos/urls.py
+++ photos/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     #path('profile/<str:pk>', views.profile, name='profile'),
+    path('settings', views.settings, name='settings'),
     path('signup', views.signup, name='signup'),
     path('signin', views.signin, name='signin'),
 ]
